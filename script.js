296e16174a95c6ea3d7b14211d43d65c4c8c393a
N-Deepika <deepika282002@gmail.com>
2022-01-12 20:05:44 +0530

    bill split

---
+++ script.js
@@ -0,0 +1,21 @@
+const splitting = () => {
+  let total = parseInt(document.querySelector('#total').value);
+  let people = parseInt(document.querySelector('#people').value);
+ 
+    const result = total / people;
+    document.querySelector('#perPerson').innerHTML = result.toFixed(2);
+}
+
+
+
+const splitBtn = document.querySelector('#splitBtn')
+  splitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    splitting();
+})
+
+document.querySelector('#resetBtn').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('form').reset();
+  document.querySelector('#perPerson').innerHTML = "0"
+})
